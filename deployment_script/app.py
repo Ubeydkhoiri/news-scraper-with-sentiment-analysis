866f397ca61a69e628e1d05559875466e370451e
Ubeydkhoiri <ubeydkhoiri@gmail.com>
2024-01-09 08:45:43 +0700

    update repo

--- deployment_script/app.py
+++ deployment_script/app.py
@@ -9,6 +9,7 @@ CUR_DIR = Path(__file__).resolve().parent
 app = Flask(__name__)
 
 dataframe = pd.read_csv(f'{CUR_DIR}/data/data.csv')
+dataframe = dataframe.sort_values(['date'], ascending=False)
 
 @app.route('/')
 def home():
@@ -26,14 +27,14 @@ def export():
     if df.to_dict(orient='records') == []:
         return {
             'keyword': keyword,
-            'message':'Data is empty, {keyword} is not presidential candidate'}
+            'message':f'Data is empty, {keyword} is not presidential candidate'}
 
     return df.to_dict(orient='records')
 
 @app.route('/updatedata')
 def updatedata():
     main()
-    return 'Data has been updated!'
+    return {'message':'Data has been updated!'}
 
 if __name__ == '__main__':
     app.run(debug=True)
